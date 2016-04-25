da7a27e7c35789beee36ac4ac2c01ca1394a2859
Elsa Schröder <elsa.schroder@gmail.com>
2016-04-25 12:04:39 +0200

    Added Lab-04

---
+++ Lab-04/src/lab/WebCrawler.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class WebCrawler {
+	
+	{
+	    // Fields
+	    private static final int MAX_PAGES_TO_SEARCH = 10;
+	    private Set<String> pagesVisited = new HashSet<String>();
+	    private List<String> pagesToVisit = new LinkedList<String>();
+	}
+}
