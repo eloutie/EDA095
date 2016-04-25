b6a8146256467b8893cca4fe4411ad439cdf4ea3
Elsa Schröder <elsa.schroder@gmail.com>
2016-04-25 09:32:46 +0200

    Switched place of while and try

--- Lab-03/src/Main.java
+++ Lab-03/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Vilken port vill du ansluta till? ");
-		//int port = scan.nextInt();
+		int port = scan.nextInt();
 		scan.close();
 		try {
 			TCPServer t = new TCPServer();
